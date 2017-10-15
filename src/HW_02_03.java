355b8997d34e9ff74383a3b43a641642011eee93
JavaPractice <forint@yandex.ru>
2017-10-15 08:30:08 +0300

    4

--- src/HW_02_03.java
+++ src/HW_02_03.java
@@ -4,6 +4,17 @@
 */
 public class HW_02_03 {
     public static void main(String[] args) {
+        double a = 0.15, b = 6.75, c = 18.2;
+        //double a = 7, b = 8, c = 11;
+        double root1, root2;
 
+        root1 = (b*(-1) + Math.sqrt(b*b - 4*a*c))/(2*a);
+        root2 = (b*(-1) - Math.sqrt(b*b - 4*a*c))/(2*a);
+
+        if (Double.isNaN(root1) && Double.isNaN(root1)) {
+            System.out.println("Корней нет");
+        } else {
+            System.out.println("Корни квадратного уравнения равны " + root1 + " и " + root2);
+        }
     }
 }
