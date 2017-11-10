e65c2c63f1a5089d065a6bd9fc37d976033ebad8
RPetrovKSRC <32666904+RPetrovKSRC@users.noreply.github.com>
2017-11-10 16:03:46 +0300

    Update Product.java

--- src/shop/Product.java
+++ src/shop/Product.java
@@ -1,9 +1,23 @@
 package shop;
 
 public class Product {
-    String id;
+    int id;
     String title;
     String description;
     int qty;
     double price;
+
+    public Product(int id, String title, String description, int qty, double price) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.qty = qty;
+        this.price = price;
+    }
+    
+    @Override
+    public String toString(){
+        return id + " - " + title + " (" + description + ")";
+    }
+    
 }
