c42152a2bf595ad4019e2d7ae3fc46027e9ecdfe
RPetrovKSRC <32666904+RPetrovKSRC@users.noreply.github.com>
2017-11-10 16:03:17 +0300

    Update User.java

--- src/shop/User.java
+++ src/shop/User.java
@@ -3,5 +3,30 @@ package shop;
 public class User {
     String name;
     String password;
-    String account;
+    double account;
+    
+    public User (String name, String password) {
+        this.name = name;
+        this.password = password;
+        account = 0;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        User user = (User) o;
+        return this.name.equals(user.name);
+    }
+    
+    @Override
+    public int hashCode() {
+        //String namepass = this.name + this.password;
+        //return namepass.hashCode();
+        return this.name.hashCode();
+    }
+    
+    @Override
+    public String toString(){
+        return "User - " + name + " (" + account + ")";
+    }
+    
 }
